ecfa4149d9b439803e5871ba2d112ea433b9cda9
Tatsuya Nishikiori <nishikiori@mba2012mid.local>
2019-05-02 13:28:26 +0900

    bug fix

--- original.js
+++ original.js
@@ -12,24 +12,35 @@ $("img").lazyload({
 });
 
 //page topボタン----------------------------------------------------------------------
-/*初期状態ではボタンが隠れている*/
-$('#back-to-top').hide();
-
-/*スクロールするとボタンが現れる*/
-$(window).scroll(function() {
-  if($(this).scrollTop() > 60) {
-    $('#back-to-top').fadeIn("slow");
-  } else {
-    $('#back-to-top').fadeOut("slow");
-  }
-});
-
-/*ボタンクリックでトップに戻る*/
-$('#back-to-top').on('click', function(){
-  $('html, body').animate({
-    'scrollTop':0
-  },500);
-  return false;
+$(document).ready(function(){
+  $("#topBtn").hide();
+  $(window).on("scroll", function() {
+    if ($(this).scrollTop() > 100) {
+      $("#topBtn").fadeIn("fast");
+    } else {
+      $("#topBtn").fadeOut("fast");
+    }
+    scrollHeight = $(document).height(); //ドキュメントの高さ 
+    scrollPosition = $(window).height() + $(window).scrollTop(); //現在地 
+    footHeight = $("footer").innerHeight(); //footerの高さ（＝止めたい位置）
+    if ( scrollHeight - scrollPosition  <= footHeight ) { //ドキュメントの高さと現在地の差がfooterの高さ以下になったら
+      $("#topBtn").css({
+        "position":"absolute", //pisitionをabsolute（親：wrapperからの絶対値）に変更
+        "bottom": footHeight + 20 //下からfooterの高さ + 20px上げた位置に配置
+      });
+    } else { //それ以外の場合は
+      $("#topBtn").css({
+        "position":"fixed", //固定表示
+        "bottom": "20px" //下から20px上げた位置に
+      });
+    }
+  });
+  $('#topBtn').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 400);
+    return false;
+  });
 });
 
 /*
